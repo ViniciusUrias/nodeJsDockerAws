0dbc155379a455d25a77704c7bf2e735e64ba5a4
Vinicius Urias de Abreu <vinicius.abreu@m2center.com.br>
2023-12-01 10:09:38 -0300

    mian

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-	res.send("TESTE AWS deployando novamente!");
+	res.send("TESTE AWS deployando novamente teste ipcifox!");
 });
 
 app.listen(port, () => {
